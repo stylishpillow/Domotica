afaec57b5c5bb3dae25227170a16ecc0d385551e
Erik van der Horst <erikvanderhorst@hotmail.com>
2020-06-01 15:10:54 +0200

    functies gemaakt en btje netter neergezet comments toegevoegd en ALLEMAAL DINGEN OOK

--- project/ProjectClient/ProjectClient/HttpReq.cs
+++ project/ProjectClient/ProjectClient/HttpReq.cs
@@ -8,20 +8,74 @@ namespace ProjectClient
 {
     class HttpReq
     {
-        public string GetHtml(int servo = -1)
-        {
-            string uri = "http://192.168.1.107/"; // url van arduino
-            if (servo > -1)
-            { 
-                uri += "?p7=" + servo; // plak get request achter url
-            }
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);    //http request
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();  //http response
-            StreamReader sr = new StreamReader(response.GetResponseStream());   //geen idee maar moeten we gebruiken 
+        private HttpWebResponse webResponse;
 
-            string html = sr.ReadToEnd();   //lees de webpagina
-            sr.Close(); // sluit connectie
-            return html; //geef de gehele html pagina terug
+        /// <summary>
+        /// Connect to website and set webresponse 
+        /// if servo is set set servo value in url
+        /// </summary>
+        /// <param name="servo"></param>
+        private void ConnectToWebsite(int servo = -1)
+        {
+            string url = "http://192.168.2.17/"; // url van arduino
+            if (servo > -1)
+            {
+                url += "?p7=" + servo; // plak get request achter url
+            }
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);    //http request
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            webResponse = response;// set webrequest 
+
         }
+        /// <summary>
+        /// get all the html from the webpage
+        /// </summary>
+        /// <returns>all the html from the website</returns>
+        private string GetHtml()
+        { 
+            StreamReader sr = new StreamReader(webResponse.GetResponseStream());
+
+            string html = sr.ReadToEnd();   
+            sr.Close(); 
+            return html; 
+        }
+        /// <summary>
+        /// Connects to website and get all the html. 
+        /// search for the <b> tag and get value after the tag (servo value)
+        /// </summary>
+        /// <returns>servo value</returns>
+        public string GetServoValue()
+        {
+            ConnectToWebsite();
+            string htmlPage = GetHtml();
+            string ServoValue = htmlPage.Substring(htmlPage.IndexOf("<b>") + 3, 3);
+
+            return ServoValue;
+        }
+
+        /// <summary>
+        /// Connects to website and get all the html. 
+        /// search for the <i> tag and get value after the tag (Sensor value)
+        /// </summary>
+        /// <returns>Sensor value</returns>
+        public string GetSensorValue()
+        {
+            ConnectToWebsite();
+            string htmlPage = GetHtml();
+            string sensorValue = htmlPage.Substring(htmlPage.IndexOf("<i>") + 3, 3);
+
+            return sensorValue;
+        }
+
+        /// <summary>
+        /// Set the servo value in ConnectToWebsite (for the get request)
+        /// </summary>
+        /// <param name="servoValue"></param>
+        public void SetServoValue(int servoValue)
+        {
+            ConnectToWebsite(servoValue);
+        }
+       
     }
 }
